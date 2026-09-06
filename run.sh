429af4d67c2290430463fc9e1cb0ae3ecb8bfc65
Kirubakaran <kirubakaranj87@gmail.com>
2026-09-06 12:50:09 +0000

    Initial commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+export AWS_ACCESS_KEY_ID="ASIAV27FRJJ75HPSR26M"
+export AWS_SECRET_ACCESS_KEY="drn7Fk5644MOEEzCBOFMvJ67tSYRmaNigGl4P8oT"
+export AWS_DEFAULT_REGION="us-east-1"
+cd setup/terraform
+terraform apply -auto-approve
